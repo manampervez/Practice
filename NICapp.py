81cf28d0232c44250acde3dac6c276bbc022290c
manampervez <manam.pervez@gmail.com>
2019-01-09 23:10:45 -0500

    tkinter base created

--- NICapp.py
+++ NICapp.py
@@ -1,1 +1,12 @@
-print("This is testing")
+from tkinter import *
+
+window= Tk()
+window.title("TPA Software")
+
+wifiOn=Button(window,text="WiFi-ON")
+wifiOn.grid(row=0,column=0)
+
+wifiOff=Button(window,text="WiFi-OFF")
+wifiOff.grid(row=1,column=0)
+
+window.mainloop()
