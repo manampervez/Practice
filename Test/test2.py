77c84b63f401710dae117bd1f1f63b4e9863c647
manampervez <manam.pervez@gmail.com>
2019-01-14 23:33:49 -0500

    import os os.system

--- Test/test2.py
+++ Test/test2.py
@@ -1,1 +1,8 @@
-from netifaces import *
+import os
+os.system("netsh interface show interface")
+
+def enable():
+    os.system("netsh interface set interface 'Wi-Fi' enabled")
+
+def disable():
+    os.system("netsh interface set interface 'Wi-Fi' disabled")
