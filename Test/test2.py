15374573087ce5969522ae6a8b5bbcf9d7051c21
manampervez <manam.pervez@gmail.com>
2019-01-17 22:50:38 -0500

    import os os.system
    new code added

--- Test/test2.py
+++ Test/test2.py
@@ -1,15 +1,29 @@
+'''
+
 import os
-os.system("netsh interface show interface")
+#os.system("netsh interface show interface")
 
 def enable():
     os.system("netsh interface set interface 'Wi-Fi' enabled")
 
 enable()
 
-def disable():
-   os.system("netsh interface set interface 'TPALAN' disabled")
+#def disable():
+#   os.system("netsh interface set interface 'TPALAN' disabled")
 
-disable()
+#disable()
 
-print(enable())
-print(disable())
+'''
+
+
+
+'''
+import subprocess
+
+def enable():
+    subprocess.call('netsh interface set interface "Wi-Fi" enabled')
+enable()
+'''
+
+import subprocess
+subprocess.call(['netsh', 'interface', 'set', 'interface', 'Wi-Fi', 'disabled'])
