dc31f4633dbaf3d97faf2fc3d6e8d1908e983159
manampervez <manam.pervez@gmail.com>
2019-01-17 23:10:08 -0500

    subprocess working script

--- Test/test2.py
+++ Test/test2.py
@@ -1,11 +1,15 @@
-'''
-
 import os
-#os.system("netsh interface show interface")
+import subprocess
+os.system("netsh interface show interface")
+# It will enabled Wi-Fi Network
+subprocess.call(['netsh', 'interface', 'set', 'interface', 'Wi-Fi', 'enabled'])
 
+# It will enabled TPALAN Network
+# subprocess.call(['netsh', 'interface', 'set', 'interface', 'TPALAN', 'enabled'])
+
+'''
 def enable():
     os.system("netsh interface set interface 'Wi-Fi' enabled")
-
 enable()
 
 #def disable():
@@ -15,16 +19,11 @@ enable()
 
 '''
 
-
-
 '''
 import subprocess
 
 def enable():
     subprocess.call('netsh interface set interface "Wi-Fi" enabled')
 enable()
-'''
 
-import subprocess
-subprocess.call(['netsh', 'interface', 'set', 'interface', 'Wi-Fi', 'disabled'])
-#
+'''
