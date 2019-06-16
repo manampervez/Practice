51a3922b581451490f646a146c7c1117f9358683
manampervez <manam.pervez@gmail.com>
2019-06-16 19:51:28 -0400

    LAPTOP: - Century21 practice addedd

--- Century21/century21.py
+++ Century21/century21.py
@@ -10,6 +10,31 @@ soup=BeautifulSoup(c,"html.parser")
 #print(soup.prettify())
 all=soup.find_all("div",{"class":"propertyRow"})
 all[0].find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ","")
-print(all)
+#print(all)
 #all2=all[0].find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ","")
 #print(all2)
+
+for item in all:
+    print(item.find("h4",{"class","propPrice"}).text.replace("\n","").replace(" ",""))
+    print(item.find_all("span",{"class","propAddressCollapse"})[0].text)
+    print(item.find_all("span", {"class", "propAddressCollapse"})[1].text)
+
+    try:
+        print(item.find("span",{"class","infoBed"}).find("b").text)
+    except:
+        print(None)
+
+    try:
+        print(item.find("span",{"class","infoSqFt"}).find("b").text)
+    except:
+        print(None)
+
+    try:
+        print(item.find("span",{"class","infoValueFullBath"}).find("b").text)
+    except:
+        print(None)
+    try:
+        print(item.find("span",{"class","infoValueHalfBath"}).find("b").text)
+    except:
+        print(None)
+    print(" ")
